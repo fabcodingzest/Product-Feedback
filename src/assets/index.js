306fd28bb9702225e05481d304d640b195d6b309
fabcodingzest <fabrizvi786@gmail.com>
2022-09-08 23:53:40 +0530

    exported: more icons

---
+++ src/assets/index.js
@@ -0,0 +1,4 @@
+export { default as DesktopBg } from "./suggestions/desktop/background-header.png";
+export { default as Bulb } from "./suggestions/icon-suggestions.svg";
+export { default as ArrowUp } from "./shared/icon-white-arrow-up.svg";
+export { default as Tick } from "./shared/icon-check.svg";
