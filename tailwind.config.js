132f943b3373c73209eb96deeaddf927d4fc2422
fabcodingzest <fabrizvi786@gmail.com>
2022-09-08 23:52:54 +0530

    updated: tailwind theme placement

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,18 +2,20 @@ module.exports = {
   mode: "jit",
   content: ["./src/**/*.{html,js}"],
   theme: {
-    colors: {
-      violet: "#AD1FEA",
-      blue: "#4661E6",
-      "dark-blue": "#373F68",
-      "darker-blue": "#3A4374",
-      "light-blue": "62BCFA",
-      grey: "#F2F4FF",
-      "dark-grey": "#647196",
-      bg: "#F7F8FD",
-      orange: "#F49F85",
+    extend: {
+      colors: {
+        violet: "#AD1FEA",
+        "light-purple": "#CFD7FF",
+        blue: "#4661E6",
+        "dark-blue": "#373F68",
+        "darker-blue": "#3A4374",
+        "light-blue": "#62BCFA",
+        grey: "#F2F4FF",
+        "dark-grey": "#647196",
+        bg: "#F7F8FD",
+        orange: "#F49F85",
+      },
     },
-    extend: {},
   },
   plugins: [],
 };
