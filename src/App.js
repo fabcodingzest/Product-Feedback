296b70d4ba4d9d91a57da9d1f1ae8b774e2d8faf
fabcodingzest <fabrizvi786@gmail.com>
2022-09-08 23:53:56 +0530

    updated: container layout

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { Dashboard } from "./pages";
 function App() {
   return (
     <div className=" bg-grey w-full min-h-screen">
-      <div className="container mx-auto px-28 py-14 max-w-5xl">
+      <div className="container mx-auto p-2 lg:px-8 lg:py-14 max-w-5xl">
         <Routes>
           <Route path="/" element={<Navigate to="/dashboard" />} />
           <Route path="/dashboard" element={<Dashboard />} />
