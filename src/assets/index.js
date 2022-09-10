697f3deebccbc140edd52e7931b25d9c670c6c2b
fabcodingzest <fabrizvi786@gmail.com>
2022-09-10 13:20:12 +0530

    exported: svgs as react components so they can act like svgs not images

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,4 +1,5 @@
 export { default as DesktopBg } from "./suggestions/desktop/background-header.png";
-export { default as Bulb } from "./suggestions/icon-suggestions.svg";
-export { default as ArrowUp } from "./shared/icon-white-arrow-up.svg";
-export { default as Tick } from "./shared/icon-check.svg";
+export { ReactComponent as Bulb } from "./suggestions/icon-suggestions.svg";
+export { ReactComponent as ArrowUp } from "./shared/icon-arrow-up.svg";
+export { ReactComponent as ArrowLeft } from "./shared/icon-arrow-left.svg";
+export { ReactComponent as Tick } from "./shared/icon-check.svg";
