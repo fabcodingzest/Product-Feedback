654857ce2f342206ccd9fa58aae52f85d7396274
fabcodingzest <fabrizvi786@gmail.com>
2022-09-12 18:15:42 +0530

    minor update in upvotes

--- src/components/UpVotes.js
+++ src/components/UpVotes.js
@@ -4,12 +4,13 @@ import { ArrowUp } from "../assets";
 function UpVotes({ upvotes, vertical }) {
   return (
     <div
-      className={`flex items-center justify-center bg-grey p-2.5 w-min self-start rounded-lg ${
+      className={`group flex items-center justify-center bg-grey p-2.5 w-min self-start rounded-lg ${
         vertical ? "flex-col" : ""
-      } `}>
-      <ArrowUp className="text-blue" />
+      } hover:bg-light-purple active:bg-blue cursor-pointer`}>
+      {/* Need to add onClick to handle upvoting and change styles for active ones */}
+      <ArrowUp className="text-blue group-active:text-white " />
       <p
-        className={`text-xs font-bold text-darker-blue ${
+        className={`text-xs font-bold text-darker-blue group-active:text-white ${
           vertical ? "mt-2" : "ml-1.5"
         }`}>
         {upvotes || 0}
