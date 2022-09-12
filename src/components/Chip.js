bb9c90f7d4b480eb3e38f50da10eb405cd15b0aa
fabcodingzest <fabrizvi786@gmail.com>
2022-09-12 18:10:04 +0530

    updated: chip component

--- src/components/Chip.js
+++ src/components/Chip.js
@@ -1,14 +1,12 @@
-const Chip = ({ text, filter, setFilter }) => {
+const Chip = ({ text, filter, onClick }) => {
   return (
     <div
-      className={`text-sm font-medium px-4 py-1  bg-grey rounded-xl hover:bg-light-purple cursor-pointer ${
+      className={`text-sm font-medium px-4 py-1  bg-grey rounded-lg hover:bg-light-purple cursor-pointer ${
         filter && filter === text
           ? "bg-blue text-white hover:bg-blue"
           : "text-blue"
       } active:bg-blue active:text-white`}
-      onClick={(e) => {
-        setFilter(text);
-      }}>
+      onClick={onClick}>
       {text}
     </div>
   );
