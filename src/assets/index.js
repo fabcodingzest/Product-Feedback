99bfc96e0ce860898f9fe4149a37467269c0dd9f
fabcodingzest <fabrizvi786@gmail.com>
2022-09-14 19:07:24 +0530

    added: empty icon import

--- src/assets/index.js
+++ src/assets/index.js
@@ -5,3 +5,4 @@ export { ReactComponent as ArrowDown } from "./shared/icon-arrow-down.svg";
 export { ReactComponent as ArrowLeft } from "./shared/icon-arrow-left.svg";
 export { ReactComponent as Tick } from "./shared/icon-check.svg";
 export { ReactComponent as Comment } from "./shared/icon-comments.svg";
+export { ReactComponent as EmptyIllustration } from "./suggestions/illustration-empty.svg";
