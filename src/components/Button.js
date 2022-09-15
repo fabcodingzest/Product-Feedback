cf3adf9b7001653c40d0aa84b4f680f71a935484
fabcodingzest <fabrizvi786@gmail.com>
2022-09-15 22:27:26 +0530

    update: py

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,18 +2,18 @@ import React from "react";
 import { ArrowLeft } from "../assets";
 import { btnColors } from "../utilities/constants";
 
-function Button({ bg, text, color, goBack }) {
+function Button({ bg, text, color, goBack, classes }) {
   return (
     <button
       className={`${
         bg && !goBack
-          ? `${btnColors[color]} text-white hover:opacity-70 px-5 py-2`
+          ? `${btnColors[color]} text-white hover:opacity-70 px-5 py-3`
           : `${
               goBack && bg
                 ? "bg-dark-blue text-white"
                 : "bg-transparent text-dark-grey"
             } hover:underline px-7 py-3`
-      } rounded-lg text-sm font-semibold flex items-center gap-3`}>
+      } rounded-lg text-sm font-semibold flex items-center gap-3 ${classes}`}>
       {goBack && (
         <ArrowLeft className={`${bg && goBack ? "text-grey" : "text-blue"}`} />
       )}
