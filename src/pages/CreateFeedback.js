814c21e866c4496cf6b79f8c035fbcbc3d3fce7e
fabcodingzest <fabrizvi786@gmail.com>
2022-09-15 22:29:31 +0530

    initiated: create feedback page

---
+++ src/pages/CreateFeedback.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Button from "../components/Button";
+
+function CreateFeedback() {
+  return (
+    <div className="max-w-md mx-auto">
+      <Button text="Go Back" goBack classes="px-0" />
+      <div className="bg-white p-2">fsdu</div>
+    </div>
+  );
+}
+
+export default CreateFeedback;
